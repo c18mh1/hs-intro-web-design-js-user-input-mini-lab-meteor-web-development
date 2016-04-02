8a29cc617706f2d89342c010541f11f8c326c182
c18mh1 <c18mh1@dalton.org>
2016-04-02 17:48:28 +0000

    Done.

---
+++ js/trip.js
@@ -0,0 +1,10 @@
+var trip_length = prompt ("How many nights will you be in NYC for?");
+var hotel = prompt ("What hotel would you like to stay at?");
+var sites = prompt ("What sites would you like to see in NYC? Please use proper grammar.");
+var food = prompt ("What food would you like to eat? Please use proper grammar.");
+
+
+alert("Itinerary: You will be staying in NYC for " + trip_length + " nights at " + hotel + ". You will be visiting " + sites + " and you will be eating " + food + ".");
+
+
+
